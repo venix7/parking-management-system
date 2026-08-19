af2ab6b5fab63524e60f31a998b38c023a1a5cf1
Uday Banerjee <udayx4@gmail.com>
2026-08-19 22:41:25 +0530

    Configure Streamlit API URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import os
 import requests
 import streamlit as st
 
-API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+try:
+    API_URL = st.secrets["API_URL"]
+except st.errors.StreamlitSecretNotFoundError:
+    API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(
     page_title="Parking Management System",
