100224aafa0af862f68a25573dcc4c12095644ba
Uday Banerjee <udayx4@gmail.com>
2026-08-19 22:26:35 +0530

    Connect Streamlit to deployed API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
+import os
 import requests
 import streamlit as st
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(
     page_title="Parking Management System",
